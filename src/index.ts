c13a1cec170f36b955f36dc0b01d7158fbb3c927
Julian-H-J-Koch <julian.koch@hs-duesseldorf.de>
2026-03-30 16:50:13 +0200

    JK#1.3.4: Type mismatch fixed

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-export { ConfirmationBox, ConfirmationBoxState, defaultConfirmationState } from './ConfirmationBox.jsx';
-export { Dropdown, DropdownOption } from './Dropdown.jsx';
-export { InfoOverlay, InfoOverlayState, defaultInfoOverlayState } from './InfoOverlay.jsx';
-export { InfoOverlayWithInput, InfoOverlayWithInputState, defaultInfoOverlayWithInputState } from './InfoOverlayWithInput.jsx';
-export { setInputFilter } from './InputFilter.jsx';
-export { LoadingOverlay, LoadingOverlayState, defaultLoadingOverlayState } from './LoadingOverlay.jsx';
-export { MultipleChoiceOverlay, MultipleChoiceOverlayState, defaultMultipleChoiceState } from './MultipleChoiceOverlay.jsx';
-export { MultipleRadioOverlay, MultipleRadioOverlayState, defaultMultipleRadioState } from './MultipleRadioOverlay.jsx';
-export { Toast } from './Toast.jsx';
-export { ToggleSwitch, ToggleSwitchOption } from './ToggleSwitch.jsx'
+export { ConfirmationBox, ConfirmationBoxState, defaultConfirmationState } from './ConfirmationBox.js';
+export { Dropdown, DropdownOption } from './Dropdown.js';
+export { InfoOverlay, InfoOverlayState, defaultInfoOverlayState } from './InfoOverlay.js';
+export { InfoOverlayWithInput, InfoOverlayWithInputState, defaultInfoOverlayWithInputState } from './InfoOverlayWithInput.js';
+export { setInputFilter } from './InputFilter.js';
+export { LoadingOverlay, LoadingOverlayState, defaultLoadingOverlayState } from './LoadingOverlay.js';
+export { MultipleChoiceOverlay, MultipleChoiceOverlayState, defaultMultipleChoiceState } from './MultipleChoiceOverlay.js';
+export { MultipleRadioOverlay, MultipleRadioOverlayState, defaultMultipleRadioState } from './MultipleRadioOverlay.js';
+export { Toast } from './Toast.js';
+export { ToggleSwitch, ToggleSwitchOption } from './ToggleSwitch.js'
